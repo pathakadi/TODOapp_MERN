15fd3d55457d1d3f0bbb6e8dce3c372859b22826
Aditya Pathak <99814487+pathakadi@users.noreply.github.com>
2023-12-02 18:52:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 import { app } from "./app.js";
 import { connectDB } from "./data/database.js";
-import cors from "cors";
 
 connectDB();
-app.use(cors());
 
 app.listen(process.env.PORT , () => {
     console.log(`Server is Working on Port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`);
