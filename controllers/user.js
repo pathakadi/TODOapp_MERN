d6f3ff03157f5690d789687022d4a017c5954137
Aditya Pathak <99814487+pathakadi@users.noreply.github.com>
2023-12-03 14:23:37 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -3,19 +3,20 @@ import bcrypt from "bcrypt";
 import { sendCookie } from "../utils/features.js";
 import ErrorHandler from "../middlewares/error.js";
 
-export const login = async(req,res,next) => {
-    try {
-        const { email , password } = req.body;
-        const user = await User.findOne({email}).select("+password");
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) return next(new ErrorHandler("Invalid Email or Password", 400));
+    const isMatch = await bcrypt.compare(password, user.password);
 
-        if(!user) return next(new ErrorHandler("Invalid Email or Password" , 400));
-        const isMatch = bcrypt.compare(password , user.password);
-        if(!isMatch) return next(new ErrorHandler("Invalid Email or Password" , 400));
+    if (!isMatch)
+      return next(new ErrorHandler("Invalid Email or Password", 400));
 
-        sendCookie(user , res , `Welcome Back , ${user.name}` , 200);
-    } catch (error) {
-        next(error);
-    }
+    sendCookie(user, res, `Welcome back, ${user.name}`, 200);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const register = async(req,res) => {
