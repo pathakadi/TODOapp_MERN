e76fa548277f80a6fbee4ed5fe9944da32551737
Aditya Pathak <99814487+pathakadi@users.noreply.github.com>
2023-12-03 16:16:57 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -6,8 +6,11 @@ import ErrorHandler from "../middlewares/error.js";
 export const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
+
     const user = await User.findOne({ email }).select("+password");
+
     if (!user) return next(new ErrorHandler("Invalid Email or Password", 400));
+
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch)
@@ -19,38 +22,41 @@ export const login = async (req, res, next) => {
   }
 };
 
-export const register = async(req,res) => {
-    try {
-        const {name , email , password} = req.body;
-        let user = await User.findOne({email});
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-        if(user) return next(new ErrorHandler("User Already Exist" , 400));
+    let user = await User.findOne({ email });
 
-        const hashedPassword = await bcrypt.hash(password , 10);
+    if (user) return next(new ErrorHandler("User Already Exist", 400));
 
-        user = await User.create({name , email , password : hashedPassword});
-        
-        sendCookie(user , res , "Registered Succesfully" , 201);
-    } catch (error) {
-        next(error);
-    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = await User.create({ name, email, password: hashedPassword });
+
+    sendCookie(user, res, "Registered Successfully", 201);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const logout = (req,res) => {
-    res.status(200).cookie("token" , "" , {
-        expires : new Date(Date.now()) ,
-        sameSite : process.env.NODE_ENV === "Development" ? "lax" : "none",
-        secure : process.env.NODE_ENV === "Development" ? false :  true
+export const getMyProfile = (req, res) => {
+  res.status(200).json({
+    success: true,
+    user: req.user,
+  });
+};
+
+export const logout = (req, res) => {
+  res
+    .status(200)
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      sameSite: process.env.NODE_ENV === "Develpoment" ? "lax" : "none",
+      secure: process.env.NODE_ENV === "Develpoment" ? false : true,
     })
-        .json({
-        success : true ,
-        user : req.user
-    });
-};
-
-export const getMyProfile = (req,res) => {
-    res.status(200).json({
-        success : true ,
-        user : req.user
+    .json({
+      success: true,
+      user: req.user,
     });
 };
